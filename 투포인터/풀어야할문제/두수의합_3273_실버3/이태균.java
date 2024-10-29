0405c50632026f4d694804fd4551a1915bcd33c6
taegyun1995 <needle12s@gmail.com>
2024-10-29 19:50:19 +0900

    투 포인터 : 두 수의 합

---
+++ 투포인터/풀어야할문제/두수의합_3273_실버3/이태균.java
@@ -0,0 +1,55 @@
+package 투포인터.풀어야할문제.두수의합_3273_실버3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class 이태균 {
+
+    public static int N;
+    public static int[] ARR;
+    public static int X;
+
+    public static int COUNT = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        ARR = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            ARR[i] = Integer.parseInt(st.nextToken());
+        }
+
+        X = Integer.parseInt(br.readLine());
+
+        Arrays.sort(ARR);
+
+        two_pointer();
+
+        System.out.println(COUNT);
+    }
+
+    private static void two_pointer() {
+        int left = 0;
+        int right = N - 1;
+
+        while (left < right) {
+            int sum = ARR[left] + ARR[right];
+
+            if (sum == X) {
+                COUNT++;
+                left++;
+                right--;
+            } else if (sum < X) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+}
