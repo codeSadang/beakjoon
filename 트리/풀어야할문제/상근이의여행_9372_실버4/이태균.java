57652a1bfa082339ac587371c9d17c0140fd526a
taegyun1995 <needle12s@gmail.com>
2024-11-09 11:20:42 +0900

    트리 : 상근이의 여행

--- 트리/풀어야할문제/상근이의여행_9372_실버4/이태균.java
+++ 트리/풀어야할문제/상근이의여행_9372_실버4/이태균.java
@@ -3,7 +3,8 @@ package 트리.풀어야할문제.상근이의여행_9372_실버4;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class 이태균 {
@@ -11,27 +12,50 @@ public class 이태균 {
     public static int T;
     public static int N;
     public static int M;
+    public static List<List<Integer>> AIRPLANE_LIST;
+    public static boolean[] VISITED;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         T = Integer.parseInt(br.readLine());
         StringTokenizer st;
+
         for (int i = 0; i < T; i++) {
             st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
             M = Integer.parseInt(st.nextToken());
 
-            int[][] airplane_list = new int[M][2];
+            AIRPLANE_LIST = new ArrayList<>();
+            VISITED = new boolean[N + 1];
+
+            for (int j = 0; j <= N; j++) {
+                AIRPLANE_LIST.add(new ArrayList<>());
+            }
+
             for (int j = 0; j < M; j++) {
                 st = new StringTokenizer(br.readLine());
-                airplane_list[j][0] = Integer.parseInt(st.nextToken());
-                airplane_list[j][1] = Integer.parseInt(st.nextToken());
+                int u = Integer.parseInt(st.nextToken());
+                int v = Integer.parseInt(st.nextToken());
+
+                AIRPLANE_LIST.get(u).add(v);
+                AIRPLANE_LIST.get(v).add(u);
             }
-            System.out.println(Arrays.deepToString(airplane_list));
+
+            int result = dfs(1, 0);
+            System.out.println(result);
         }
     }
 
-    private static void dfs() {
+    private static int dfs(int now, int count) {
+        VISITED[now] = true;
+
+        for (int next : AIRPLANE_LIST.get(now)) {
+            if (!VISITED[next]) {
+                count = dfs(next, count + 1);
+            }
+        }
+
+        return count;
     }
 }
