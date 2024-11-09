c6d4c2d196620a7c95d8c498fe561782c7a3ee95
taegyun1995 <needle12s@gmail.com>
2024-11-09 12:02:32 +0900

    트리 : 풉시다 이거

---
+++ 트리/풀어야할문제/나무탈출_15900_실버1/이태균.java
@@ -0,0 +1,16 @@
+package 트리.풀어야할문제.나무탈출_15900_실버1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 이태균 {
+
+    public static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+    }
+
+}
