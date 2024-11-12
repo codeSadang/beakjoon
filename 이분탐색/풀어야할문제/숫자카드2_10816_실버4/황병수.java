0e96492db58b8e9db6c0cc2641ea491b9021bd48
choonsik <moodeary@gmail.com>
2024-11-12 21:01:20 +0900

    update

--- 이분탐색/풀어야할문제/숫자카드2_10816_실버4/황병수.java
+++ 이분탐색/풀어야할문제/숫자카드2_10816_실버4/황병수.java
@@ -1,4 +1,73 @@
 package 이분탐색.풀어야할문제.숫자카드2_10816_실버4;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ *
+ */
 public class 황병수 {
+
+    static int N,M;
+    static int[] AList, BList;
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        AList = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            AList[i] = Integer.parseInt(st.nextToken());
+        }
+
+        M = Integer.parseInt(br.readLine());
+
+        BList = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            BList[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(AList);
+
+        StringBuilder sb = new StringBuilder();
+        for (int curNum : BList) {
+            int left = 0;
+            int right = N-1;
+            int count = 0;
+
+            while (left < right) {
+                int mid = (left + right)/2;
+                if (AList[mid] == curNum) {
+                    count++;
+
+                    int Rmid = mid+1;
+                    int Lmid = mid-1;
+                    while (AList[Rmid] == curNum && Rmid < N) {
+                        Rmid++;
+                        count++;
+                        if (Rmid == N) break;
+                    }
+
+                    while (AList[Lmid] == curNum) {
+                        Lmid--;
+                        count++;
+                        if (Lmid == -1) break;
+                    }
+
+                    break;
+                } else if (AList[mid] < curNum) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+            sb.append(count).append(" ");
+        }
+        System.out.println(sb);
+    }
 }
