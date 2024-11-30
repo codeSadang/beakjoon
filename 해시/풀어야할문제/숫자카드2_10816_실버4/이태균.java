8e7ead7c0b13219d7c6601a0770cc3aa206f9591
taegyun1995 <needle12s@gmail.com>
2024-11-30 19:29:33 +0900

    해시 : 숫자카드2

---
+++ 해시/풀어야할문제/숫자카드2_10816_실버4/이태균.java
@@ -0,0 +1,38 @@
+package 해시.풀어야할문제.숫자카드2_10816_실버4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class 이태균 {
+
+    public static int N;
+    public static Map<Integer, Integer> N_MAP;
+    public static int M;
+
+    public static StringBuilder SB = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        N_MAP = new HashMap<>(N);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int card = Integer.parseInt(st.nextToken());
+            N_MAP.put(card, N_MAP.getOrDefault(card, 0) + 1);
+        }
+
+        M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int count = Integer.parseInt(st.nextToken());
+            SB.append(N_MAP.getOrDefault(count, 0)).append(" ");
+        }
+
+        System.out.println(SB.toString().trim());
+    }
+}
