19c666312bac77c530eb731ddd9d8a5ddeb82641
taegyun1995 <needle12s@gmail.com>
2024-12-01 13:20:02 +0900

    해시 : 문자열 집합

---
+++ 해시/풀어야할문제/문자열집합_14425_실버4/이태균.java
@@ -0,0 +1,39 @@
+package 해시.풀어야할문제.문자열집합_14425_실버4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class 이태균 {
+
+    public static int N;
+    public static int M;
+    public static Set<String> S_SET;
+
+    public static int RESULT = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        S_SET = new HashSet<>(N);
+        for (int i = 0; i < N; i++) {
+            S_SET.add(br.readLine());
+        }
+
+        for (int i = 0; i < M; i++) {
+            if (S_SET.contains(br.readLine())) {
+                RESULT++;
+            }
+        }
+
+        System.out.println(RESULT);
+    }
+
+}
