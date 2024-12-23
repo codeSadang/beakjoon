695e817c07c4eb0ef2c49cabce8188e808e42aae
taegyun1995 <needle12s@gmail.com>
2024-12-23 21:44:26 +0900

    그리디 : 카드 정렬하기

--- 그리디/풀어야할문제/카드정렬하기_1715_골드4/이태균.java
+++ 그리디/풀어야할문제/카드정렬하기_1715_골드4/이태균.java
@@ -3,39 +3,32 @@ package 그리디.풀어야할문제.카드정렬하기_1715_골드4;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.PriorityQueue;
 
 public class 이태균 {
-
-    public static int N;
-    public static int[] CARD_LIST;
-
-    public static List<Integer> RESULT = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        N = Integer.parseInt(br.readLine());
-        CARD_LIST = new int[N];
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
         for (int i = 0; i < N; i++) {
-            CARD_LIST[i] = Integer.parseInt(br.readLine());
-        }
-        Arrays.sort(CARD_LIST);
-
-        int temp = CARD_LIST[0] + CARD_LIST[1];
-        RESULT.add(temp);
-
-        for (int i = 2; i < CARD_LIST.length; i++) {
-            temp += CARD_LIST[i];
-            RESULT.add(temp);
+            pq.add(Integer.parseInt(br.readLine()));
         }
 
-        int sum = RESULT.stream().mapToInt(Integer::intValue).sum();
-        System.out.println(sum);
+        int totalCost = 0;
+
+        while (pq.size() > 1) {
+            int first = pq.poll();
+            int second = pq.poll();
+
+            int mergeCost = first + second;
+            totalCost += mergeCost;
+
+            pq.add(mergeCost);
+        }
+
+        System.out.println(totalCost);
     }
-
 }
