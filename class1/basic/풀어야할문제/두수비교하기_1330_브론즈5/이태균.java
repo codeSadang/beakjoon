067e8a6d68e2c6d62b0c63370f2ecc3fa65d8bc2
taegyun1995 <needle12s@gmail.com>
2025-01-03 22:34:40 +0900

    Class Lv1 Base Step : 두 수 비교하기

---
+++ class1/basic/풀어야할문제/두수비교하기_1330_브론즈5/이태균.java
@@ -0,0 +1,26 @@
+package class1.basic.풀어야할문제.두수비교하기_1330_브론즈5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 이태균 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        if (A > B) {
+            System.out.println(">");
+        } else if (A < B) {
+            System.out.println("<");
+        } else {
+            System.out.println("==");
+        }
+    }
+
+}
