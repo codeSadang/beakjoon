c5943f9a87fa4a8a8be0463acdfcaf19770a3cae
taegyun1995 <needle12s@gmail.com>
2025-01-03 22:44:15 +0900

    Class Lv1 Base Step : 별 찍기 -1

--- class1/basic/풀어야할문제/별찍기1_2438_브론즈5/이태균.java
+++ class1/basic/풀어야할문제/별찍기1_2438_브론즈5/이태균.java
@@ -1,2 +1,24 @@
-package class1.basic.풀어야할문제.별찍기1_2438_브론즈5;public class 이태균 {
+package class1.basic.풀어야할문제.별찍기1_2438_브론즈5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 이태균 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            for (int j = 1; j <= i + 1; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
 }
