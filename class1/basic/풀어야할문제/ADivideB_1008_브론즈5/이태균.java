1dc42382b4b019df1115a2417ec6433572948738
taegyun1995 <needle12s@gmail.com>
2025-01-03 22:31:22 +0900

    Class Lv1 Base Step : A / B

---
+++ class1/basic/풀어야할문제/ADivideB_1008_브론즈5/이태균.java
@@ -0,0 +1,20 @@
+package class1.basic.풀어야할문제.ADivideB_1008_브론즈5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 이태균 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        double A = Double.parseDouble(st.nextToken());
+        double B = Double.parseDouble(st.nextToken());
+
+        System.out.println(A / B);
+    }
+
+}
