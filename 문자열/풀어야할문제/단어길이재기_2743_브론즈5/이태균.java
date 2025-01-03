39b2b5ec7a579615b9320773e46ba8ee005fea7b
gyunny <needle12s@gmail.com>
2025-01-03 15:43:30 +0900

    문자열 : 단어 길이 재기

---
+++ 문자열/풀어야할문제/단어길이재기_2743_브론즈5/이태균.java
@@ -0,0 +1,18 @@
+package 문자열.풀어야할문제.단어길이재기_2743_브론즈5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 이태균 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        int length = str.length();
+
+        System.out.println(length);
+    }
+
+}
