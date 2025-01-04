c7723b046247b1670908bbce754f98e89025b0cd
taegyun1995 <needle12s@gmail.com>
2025-01-04 20:27:06 +0900

    Class Lv1 Base Step : 개

---
+++ class1/풀어야할문제/개_10172_브론즈5/이태균.java
@@ -0,0 +1,13 @@
+package class1.풀어야할문제.개_10172_브론즈5;
+
+public class 이태균 {
+
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.print("||_/=\\\\__|");
+    }
+
+}
