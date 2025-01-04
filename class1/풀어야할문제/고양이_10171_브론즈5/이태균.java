a24ddcc500e2c3253dce8fe41231b301cdffa9dd
taegyun1995 <needle12s@gmail.com>
2025-01-04 20:25:44 +0900

    Class Lv1 Base Step : 고양이

---
+++ class1/풀어야할문제/고양이_10171_브론즈5/이태균.java
@@ -0,0 +1,12 @@
+package class1.풀어야할문제.고양이_10171_브론즈5;
+
+public class 이태균 {
+
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.print(" \\(__)|");
+    }
+
+}
