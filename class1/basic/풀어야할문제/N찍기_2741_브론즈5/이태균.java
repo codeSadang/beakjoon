cad0d47476e2ca62eb75e035668671dc0fc92531
taegyun1995 <needle12s@gmail.com>
2025-01-04 20:22:27 +0900

    Class Lv1 Base Step : N 찍기

---
+++ class1/basic/풀어야할문제/N찍기_2741_브론즈5/이태균.java
@@ -0,0 +1,18 @@
+package class1.basic.풀어야할문제.N찍기_2741_브론즈5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 이태균 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
