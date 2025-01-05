9ecf02666a5d57d76267b1bc82d1533c269249aa
jiyeon <journiyoon@gmail.com>
2025-01-05 20:34:09 +0900

    구간합구하기5-윤지

---
+++ 다이나믹프로그래밍/풀어야할문제/구간합구하기5_11660_실버1/윤지연.java
@@ -0,0 +1,49 @@
+package 다이나믹프로그래밍.풀어야할문제.구간합구하기5_11660_실버1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 윤지연 {
+    private static int[][] arr;
+    private static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        arr = new int[N+1][N+1];
+        dp = new int[N+1][N+1];
+        // 1. 원본 배열 입력
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        // 2. dp 배열 입력
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + arr[i][j];
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int startRow = Integer.parseInt(st.nextToken());
+            int startCol = Integer.parseInt(st.nextToken());
+            int endRow = Integer.parseInt(st.nextToken());
+            int endCol = Integer.parseInt(st.nextToken());
+            intervalSum(startRow, startCol, endRow, endCol);
+        }
+    }
+
+    private static void intervalSum(int startRow, int startCol, int endRow, int endCol) {
+        int sum = 0;
+        sum = dp[endRow][endCol] - dp[startRow-1][endCol] - dp[endRow][startCol-1] + dp[startRow-1][startCol-1];
+        System.out.println(sum);
+    }
+}
