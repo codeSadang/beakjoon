3764534cb28aec94cb73b1f56b22fb6268d7d949
HongChangMo <hcm7402@naver.com>
2025-01-08 22:40:08 +0900

    update

--- 다이나믹프로그래밍/풀어야할문제/평범한배낭_12865_골드5/홍창모2.java
+++ 다이나믹프로그래밍/풀어야할문제/평범한배낭_12865_골드5/홍창모2.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class 홍창모 {
+public class 홍창모2 {
 
     static int N, K;
     public static void main(String[] args) throws IOException {
