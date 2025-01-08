ed5261d57cb7b702849e1af38bb9d2851726e805
HongChangMo <hcm7402@naver.com>
2025-01-08 22:34:39 +0900

    update

--- 다이나믹프로그래밍/풀어야할문제/구간합구하기5_11660_실버1/홍창모2.java
+++ 다이나믹프로그래밍/풀어야할문제/구간합구하기5_11660_실버1/홍창모2.java
@@ -4,15 +4,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-/*
-* 다시 풀어보기
-* */
-public class 홍창모 {
+
+public class 홍창모2 {
 
     static int N, M;
-    static int[][] board;
-    static int[][] prefixSum;
 
+    static int[][] board, prefixSumBoard;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -21,23 +18,33 @@ public class 홍창모 {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        board = new int[N + 1][N + 1];
-        prefixSum = new int[N + 1][N + 1];
+        // 2차 배열 초기화
+        board = new int[N+1][N+1];
+        // 누적합을 담을 2차 배열 초기화
+        prefixSumBoard = new int[N+1][N+1];
 
-        // 숫자판 세팅
-        for (int i = 1; i <= N; i++) {
+        // board 값 세팅
+        for( int i = 1; i<= N; i++ ) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= N; j++) {
+
+            for( int j = 1; j<= N; j++ ) {
                 board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        // 2차원 누적 합 배열 계산
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
-                prefixSum[i][j] = board[i][j] + prefixSum[i - 1][j] + prefixSum[i][j - 1] - prefixSum[i - 1][j - 1];
+        // 누적합 배열 세팅
+        for( int i = 1; i <= N; i++ ) {
+            for( int j = 1; j<=N; j++ ) {
+                prefixSumBoard[i][j] = board[i][j] + prefixSumBoard[i-1][j] + prefixSumBoard[i][j-1] - prefixSumBoard[i-1][j-1];
             }
         }
+
+        /*for( int i = 1; i <= N; i++ ) {
+            for ( int j = 1; j <= N; j++ ) {
+                System.out.print(prefixSumBoard[i][j] + " ");
+            }
+            System.out.println();
+        }*/
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
@@ -53,6 +60,7 @@ public class 홍창모 {
     }
 
     private static int getSum(int x1, int y1, int x2, int y2) {
-        return prefixSum[x2][y2] - prefixSum[x1 - 1][y2] - prefixSum[x2][y1 - 1] + prefixSum[x1 - 1][y1 - 1];
+
+        return prefixSumBoard[x2][y2] - prefixSumBoard[x1 - 1][y2] - prefixSumBoard[x2][y1-1] + prefixSumBoard[x1-1][y1-1];
     }
 }
---
+++ 다이나믹프로그래밍/풀어야할문제/평범한배낭_12865_골드5/홍창모.java
@@ -0,0 +1,39 @@
+package 다이나믹프로그래밍.풀어야할문제.평범한배낭_12865_골드5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 홍창모 {
+
+    static int N, K;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());       // 물품의 수
+        K = Integer.parseInt(st.nextToken());       // 버틸 수 있는 무게
+
+        // 버틸 수 있는 무게 (1~K 까지 가치를 0으로 초기화)
+        int[] dp = new int[K+1];
+
+        for( int i = 0; i < N; i++ ) {
+            st = new StringTokenizer(br.readLine());
+
+            // 물건의 무게
+            int weight = Integer.parseInt(st.nextToken());
+            // 물건의 가치
+            int value = Integer.parseInt(st.nextToken());
+
+            for( int j = K; j >= weight; j-- ) {
+                // 버틸 수 있는 최대 무게부터 물건의 무게까지 역순으로 반복
+                dp[j] = Math.max( dp[j], dp[j-weight] + value );
+            }
+        }
+
+        System.out.println(dp[K]);
+
+    }
+}
