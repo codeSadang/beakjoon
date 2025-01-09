938e9055be680af034ac298e811fdce17bca6d5c
taegyun1995 <needle12s@gmail.com>
2025-01-09 22:07:37 +0900

    Class Lv1 Base Step : 문자와 문자열

---
+++ class1/풀어야할문제/문자와문자열_27866_브론즈5/이태균.java
@@ -0,0 +1,17 @@
+package class1.풀어야할문제.문자와문자열_27866_브론즈5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 이태균 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int i = Integer.parseInt(br.readLine());
+
+        System.out.println(s.charAt(i - 1));
+    }
+
+}
