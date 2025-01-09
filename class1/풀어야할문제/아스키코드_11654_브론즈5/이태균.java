f4a8731ad8730587e9c73649a46ceaa81576610d
taegyun1995 <needle12s@gmail.com>
2025-01-09 21:57:26 +0900

    Class Lv1 Base Step : 아스키코드

---
+++ class1/풀어야할문제/아스키코드_11654_브론즈5/이태균.java
@@ -0,0 +1,16 @@
+package class1.풀어야할문제.아스키코드_11654_브론즈5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 이태균 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char c = br.readLine().charAt(0);
+
+        System.out.println((int) c);
+    }
+
+}
