79e8553de648767e3328152a892fad0fc966ef69
taegyun1995 <needle12s@gmail.com>
2025-01-09 21:41:55 +0900

    Class Lv1 Base Step : A + B - 3

---
+++ class1/풀어야할문제/APlusBMinus3_10950_브론즈5/이태균.java
@@ -0,0 +1,27 @@
+package class1.풀어야할문제.APlusBMinus3_10950_브론즈5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 이태균 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            sb.append(a + b).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+}
