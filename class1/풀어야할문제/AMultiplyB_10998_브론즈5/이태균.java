a33e19caf64304d51b74745141743124e2378aef
taegyun1995 <needle12s@gmail.com>
2025-01-09 21:51:50 +0900

    Class Lv1 Base Step : A * B

---
+++ class1/풀어야할문제/AMultiplyB_10998_브론즈5/이태균.java
@@ -0,0 +1,20 @@
+package class1.풀어야할문제.AMultiplyB_10998_브론즈5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 이태균 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        System.out.println(a * b);
+    }
+
+}
