660aca39feb3c2010223c4f573ba58177a91e8ff
taegyun1995 <needle12s@gmail.com>
2025-01-09 21:59:10 +0900

    Class Lv1 Base Step : 새싹

---
+++ class1/풀어야할문제/새싹_25083_브론즈5/이태균.java
@@ -0,0 +1,14 @@
+package class1.풀어야할문제.새싹_25083_브론즈5;
+
+public class 이태균 {
+
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.print("      |");
+    }
+
+}
