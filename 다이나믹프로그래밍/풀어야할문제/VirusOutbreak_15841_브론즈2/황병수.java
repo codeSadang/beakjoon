1c4afbc82c8d44326096e4bb5f6bd10a9887f26c
Choonsik <101438447+moodeary@users.noreply.github.com>
2025-01-13 10:26:05 +0900

    hard

--- 다이나믹프로그래밍/풀어야할문제/VirusOutbreak_15841_브론즈2/황병수.java
+++ 다이나믹프로그래밍/풀어야할문제/VirusOutbreak_15841_브론즈2/황병수.java
@@ -1,5 +1,7 @@
 package 다이나믹프로그래밍.풀어야할문제.VirusOutbreak_15841_브론즈2;
 
 public class 황병수 {
-
+    public static void main(String[] args) {
+        
+    }
 }
---
+++ 다이나믹프로그래밍/풀어야할문제/일이삼더하기_9095_실버3/황병수.java
@@ -0,0 +1,23 @@
+package 다이나믹프로그래밍.풀어야할문제.일이삼더하기_9095_실버3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 정수 4를 1 2 3 의 합으로 나타내는 방법
+ * 합을 나타낼 때에는 수를 1개이상 사용해야 함
+ */
+public class 황병수 {
+
+    static int[] ListA = new int[12];
+    public static void main(String[] args) throws IOException {
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int maxCnt = 0;
+
+    }
+}
