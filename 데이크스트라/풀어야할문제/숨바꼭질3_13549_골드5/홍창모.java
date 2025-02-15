2cca6488f6d69c613faf917ab75a775ed9702ac6
HongChangMo <hcm7402@naver.com>
2025-02-15 16:31:11 +0900

    update

--- 데이크스트라/풀어야할문제/숨바꼭질3_13549_골드5/홍창모.java
+++ 데이크스트라/풀어야할문제/숨바꼭질3_13549_골드5/홍창모.java
@@ -1,63 +1,79 @@
 package 데이크스트라.풀어야할문제.숨바꼭질3_13549_골드5;
 
-import org.w3c.dom.Node;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
 public class 홍창모 {
-    static int a,b;
 
-    static List<int[]> graph;
+    static class Node {
+        int pos, depth;
+        Node(int pos, int depth) {
+            this.pos = pos;
+            this.depth = depth;
+        }
+    }
+    static int N,K;
+
+    static List<Node>[] graph;
+    static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         // 수빈이의 위치
-        a = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
         // 동생의 위치
-        b = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
 
-        graph = new ArrayList<>();
+        graph = new ArrayList[100001];
+        visited = new boolean[100001];
 
         for(int i = 0; i <= 100000; i++) {
-            graph.add(new int[]{i,0});
+            graph[i] = new ArrayList<>();
         }
 
-        System.out.println(dijkstra(a,b));
+        System.out.println(dijkstra(N, K));
     }
 
-    private static int dijkstra(int a, int b) {
+    private static int dijkstra(int N, int K) {
+        PriorityQueue<Node> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a.depth));
+        pq.add(new Node(N, 0));
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o[1]));
+        while( !pq.isEmpty() ) {
 
-        pq.add(new int[]{a,0});
+            Node current = pq.poll();
+            int pos = current.pos;
+            int depth = current.depth;
 
-        while (!pq.isEmpty()) {
-            int[] current = pq.poll();
-
-            if(current[0] == b) {
-                return current[1];
+            if( pos == K ) {
+                return depth;
             }
 
-            if(current[0] * 2 <= 100000 && graph.get(current[0] * 2)[1] == 0) {
-                graph.get(current[0] * 2)[1] = current[1];
-                pq.add(new int[]{current[0] * 2, current[1]});
+            if (visited[pos]) {
+                continue;
+            }
+            visited[pos] = true;
+
+            if (pos * 2 <= 100000 && !visited[pos * 2]) {
+                pq.add(new Node(pos * 2, depth));
+                graph[pos * 2].add(new Node(pos, depth));
             }
 
-            if(current[0] + 1 <= 100000 && graph.get(current[0] + 1)[1] == 0) {
-                graph.get(current[0] + 1)[1] = current[1] + 1;
-                pq.add(new int[]{current[0] + 1, current[1] + 1});
+            if (pos + 1 <= 100000 && !visited[pos + 1]) {
+                pq.add(new Node(pos + 1, depth + 1));
+                graph[pos + 1].add(new Node(pos, depth + 1));
             }
 
-            if(current[0] - 1 >= 0 && graph.get(current[0] - 1)[1] == 0) {
-                graph.get(current[0] - 1)[1] = current[1] + 1;
-                pq.add(new int[]{current[0] - 1, current[1] + 1});
+            if (pos - 1 >= 0 && !visited[pos - 1]) {
+                pq.add(new Node(pos - 1, depth + 1));
+                graph[pos - 1].add(new Node(pos, depth + 1));
             }
+
         }
+
         return -1;
     }
 }
