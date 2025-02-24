28d1f1237b16d1ed6f8b67fba9c4fd9bff82ad5e
taegyun1995 <needle12s@gmail.com>
2025-02-24 19:54:05 +0900

    class4 : LCS

---
+++ class4/풀어야할문제/LCS_9251_골드5/이태균.java
@@ -0,0 +1,34 @@
+package class4.풀어야할문제.LCS_9251_골드5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 이태균 {
+
+    public static int[][] DP;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+
+        int len1 = str1.length();
+        int len2 = str2.length();
+
+        DP = new int[len1 + 1][len2 + 1];
+
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    DP[i][j] = DP[i - 1][j - 1] + 1;
+                } else {
+                    DP[i][j] = Math.max(DP[i - 1][j], DP[i][j - 1]);
+                }
+            }
+        }
+
+        System.out.println(DP[len1][len2]);
+    }
+
+}
